5d47996339165740f1ef95cdc41ed05c7eff0ed4
G R Shivaranjani <147621524+ShivaranjaniGR@users.noreply.github.com>
2026-05-23 16:23:43 +0530

    Delete spi_master_nominal_200MHz directory

--- spi_master_nominal_200MHz/fv/spi_master/read_libs.tcl
+++
@@ -1,4 +0,0 @@
-add_search_path /home/userdata/22bec0985/llm_project/baseline_designs -library -both
-read_library -liberty -both \
-    /home/userdata/22bec0985/llm_project/baseline_designs/tcbn65gpluswcz_ccs.lib
-
